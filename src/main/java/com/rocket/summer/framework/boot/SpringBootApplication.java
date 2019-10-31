226ffc803e8750fb599ab6cbe16bfa3e072f5597
jack <liujie152@hotmail.com>
2019-10-31 08:22:02 +0800

    fix_bug 启动报错

---
+++ src/main/java/com/rocket/summer/framework/boot/SpringBootApplication.java
@@ -0,0 +1,82 @@
+package com.rocket.summer.framework.boot;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.rocket.summer.framework.boot.SpringBootConfiguration;
+import com.rocket.summer.framework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import com.rocket.summer.framework.boot.autoconfigure.EnableAutoConfiguration;
+import com.rocket.summer.framework.boot.context.TypeExcludeFilter;
+import com.rocket.summer.framework.context.annotation.Bean;
+import com.rocket.summer.framework.context.annotation.ComponentScan;
+import com.rocket.summer.framework.context.annotation.ComponentScan.Filter;
+import com.rocket.summer.framework.context.annotation.Configuration;
+import com.rocket.summer.framework.context.annotation.FilterType;
+import com.rocket.summer.framework.core.annotation.AliasFor;
+
+/**
+ * Indicates a {@link Configuration configuration} class that declares one or more
+ * {@link Bean @Bean} methods and also triggers {@link EnableAutoConfiguration
+ * auto-configuration} and {@link ComponentScan component scanning}. This is a convenience
+ * annotation that is equivalent to declaring {@code @Configuration},
+ * {@code @EnableAutoConfiguration} and {@code @ComponentScan}.
+ *
+ * @author Phillip Webb
+ * @author Stephane Nicoll
+ * @since 1.2.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(excludeFilters = {
+        @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+        @Filter(type = FilterType.CUSTOM,
+                classes = AutoConfigurationExcludeFilter.class) })
+public @interface SpringBootApplication {
+
+    /**
+     * Exclude specific auto-configuration classes such that they will never be applied.
+     * @return the classes to exclude
+     */
+    @AliasFor(annotation = EnableAutoConfiguration.class, attribute = "exclude")
+    Class<?>[] exclude() default {};
+
+    /**
+     * Exclude specific auto-configuration class names such that they will never be
+     * applied.
+     * @return the class names to exclude
+     * @since 1.3.0
+     */
+    @AliasFor(annotation = EnableAutoConfiguration.class, attribute = "excludeName")
+    String[] excludeName() default {};
+
+    /**
+     * Base packages to scan for annotated components. Use {@link #scanBasePackageClasses}
+     * for a type-safe alternative to String-based package names.
+     * @return base packages to scan
+     * @since 1.3.0
+     */
+    @AliasFor(annotation = ComponentScan.class, attribute = "basePackages")
+    String[] scanBasePackages() default {};
+
+    /**
+     * Type-safe alternative to {@link #scanBasePackages} for specifying the packages to
+     * scan for annotated components. The package of each class specified will be scanned.
+     * <p>
+     * Consider creating a special no-op marker class or interface in each package that
+     * serves no purpose other than being referenced by this attribute.
+     * @return base packages to scan
+     * @since 1.3.0
+     */
+    @AliasFor(annotation = ComponentScan.class, attribute = "basePackageClasses")
+    Class<?>[] scanBasePackageClasses() default {};
+
+}
+
