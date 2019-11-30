d631ef70e4755ffe76bedaf3ebb834244a2da9e8
jack <liujie152@hotmail.com>
2019-11-30 23:59:39 +0800

    fix_bug 启动报错

---
+++ src/test/java/com/rocket/test/DemoTest.java
@@ -0,0 +1,20 @@
+package com.rocket.test;
+
+import com.rocket.summer.framework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class DemoTest {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<?> classBook = Class.forName("com.rocket.summer.framework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration$EmbeddedTomcat");
+        System.out.println(classBook);
+        Constructor<?>[] constructors = classBook.getConstructors();
+        for (int i = 0; i < constructors.length; i++) {
+            constructors[i].setAccessible(true);
+            Object o = constructors[i].newInstance();
+            System.out.println(o);
+        }
+
+    }
+}
